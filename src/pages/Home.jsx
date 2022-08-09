ce9aa28d643f5bef59930f2614509e64bf3e24d7
nggoong <nggoong@naver.com>
2022-08-09 21:07:48 +0900

    FEAT : react-query를 이용하여 데이터를 조건부 렌더링하는 기능 구현
    현재 useQuery를 이용하여 데이터를 받아오지만, 추후에 무한 스크롤을 구현하기 위해  useInfiniteQuery로 변경해야함

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useQueryClient, useQuery } from '@tanstack/react-query';
+import { roomsAPI, postingsAPI } from '../shared/api';
+import styled from 'styled-components';
 
 const Home = () => {
-
-    return(
-        <>
-        </>
-    )
+    const [isPosting, setIsPosting] = useState(false);
+    
+    const queryClient = useQueryClient();
+    const queryfetch = useQuery(['fetch_postings'], postingsAPI.fetchPostingsList, {
+      onSuccess: ({data}) => {
+        console.log(data);
+      },
+      onError: () => {
+        alert('error')
+      },
+      enabled:!isPosting,
+      staleTime:10000
+    })
+  
+    const fetchQuery = useQuery(['fetch_rooms'], roomsAPI.fetchRoomsList, {
+      onSuccess:({data}) => {
+        console.log(data);
+      },
+      enabled:isPosting,
+      staleTime:10000
+    })
+    return (
+      <div className="App">
+        <button onClick={()=>setIsPosting(true)}>rooms</button>
+        <button onClick={()=>setIsPosting(false)}>postings</button>
+        <input type="text"></input>
+        <button>제출</button>
+        {!isPosting?queryfetch.data?.data.map((item, index) => <div key={index}>{item.title}</div>):null}
+        {isPosting?fetchQuery.data?.data.map((item, index) => <div key={index}>{item.title}</div>):null}
+      </div>
+    );
 }
 
 export default Home;
