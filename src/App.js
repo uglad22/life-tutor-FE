c14566e2217990a9100c7aa0bbdbdb8d1359110b
nggoong <nggoong@naver.com>
2022-08-12 23:08:38 +0900

    FEAT : useInfiniteQuery를 이용하여 무한스크롤 기능 구현
    DB에 임시 데이터를 넣은 후, 백엔드와 통신을 통해 Home.jsx에서 무한 스크롤이 작동할 수 있도록 구현.
    게시글 카드 UI는 임시 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Home from './pages/Home';
+import instance from './shared/axios';
 
 function App() {
+
+  const tempLogin = async () => {
+    // const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/login`, {username:"a@a.a", password:"1234"});
+    // const data = res.headers;
+
+    // console.log(data);
+    localStorage.setItem("Authorization", "BEARER eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFWFBJUkVEX0RBVEUiOjE2NjA1NjMwNzUsImlzcyI6InNwYXJ0YSIsIlVTRVJfTkFNRSI6ImFAYS5hIn0.nFnnhFIj3Qq_nh8ATFUwNMC0aJ8h5EddHjjj95baebE")
+  }
   
   return (
     <div className="App">
+        <button onClick={tempLogin}>임시 로그인하기</button>
         <Home></Home>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,71 @@
 import React, { useEffect, useState } from 'react';
-import { useQueryClient, useQuery } from '@tanstack/react-query';
+import { useQueryClient, useQuery, useInfiniteQuery } from '@tanstack/react-query';
 import { roomsAPI, postingsAPI } from '../shared/api';
 import styled from 'styled-components';
+import axios from 'axios';
+import { useInView } from 'react-intersection-observer';
+import PostingCard from '../components/PostingCard';
+import instance from '../shared/axios';
+
+
 
 const Home = () => {
-    const [isPosting, setIsPosting] = useState(false);
-    
-    const queryClient = useQueryClient();
-    const queryfetch = useQuery(['fetch_postings'], postingsAPI.fetchPostingsList, {
-      onSuccess: ({data}) => {
-        console.log(data);
+  const queryClient = useQueryClient();
+  const fetchPostList = async (pageParam) => {
+    const res = await instance.get(
+      `/api/main/postings?page=${pageParam}&size=3`
+    )
+    console.log(res.data.content)
+    const { content } = res.data;
+    const { last } = res.data;
+    return { posts:content, nextPage: pageParam + 1, isLast:last}
+  }
+
+
+    const { ref, inView} = useInView();
+
+    // const { data, fetchNextPage, isFetchingNextPage, refetch } = useInfiniteQuery(
+    //   ['posts'],
+    //   ({ pageParam = 1 }) => fetchPostList(pageParam),
+    //   {
+    //     // lastPage >> 저번 페이지에서 가져온 정보
+    //     // 저번 페이지의 isLast 값이 false라면 undefined를 반환하고
+    //     // hasNextPage를 false로 설정
+    //     getNextPageParam: (lastPage) =>
+    //       !lastPage.isLast ? lastPage.nextPage : undefined,
+    //   },
+    // );
+    const { data, fetchNextPage, isFetchingNextPage, refetch } = useInfiniteQuery(
+      ['postList'],
+      ({ pageParam = 0 }) => fetchPostList(pageParam),
+      {
+        getNextPageParam: (lastPage) =>
+          !lastPage.isLast ? lastPage.nextPage : undefined,
+
+        onSuccess:(data) => {
+          console.log(data);
+        },
       },
-      onError: () => {
-        alert('error')
-      },
-      enabled:!isPosting,
-      staleTime:10000
-    })
+    );
+
+    useEffect(()=> {
+      if(inView) fetchNextPage();
+    }, [inView])    
   
-    const fetchQuery = useQuery(['fetch_rooms'], roomsAPI.fetchRoomsList, {
-      onSuccess:({data}) => {
-        console.log(data);
-      },
-      enabled:isPosting,
-      staleTime:10000
-    })
     return (
       <div className="App">
-        <button onClick={()=>setIsPosting(true)}>rooms</button>
-        <button onClick={()=>setIsPosting(false)}>postings</button>
         <input type="text"></input>
         <button>제출</button>
-        {!isPosting?queryfetch.data?.data.map((item, index) => <div key={index}>{item.title}</div>):null}
-        {isPosting?fetchQuery.data?.data.map((item, index) => <div key={index}>{item.title}</div>):null}
+        <div>
+            {data.pages?.map((page, index) => (
+              <React.Fragment key={index} >
+                  {page.posts.map((post) => (
+                    <PostingCard key={post.posting_id} post={post}></PostingCard>
+                  ))}
+              </React.Fragment>
+            ))}
+        </div>
+        {isFetchingNextPage ? <div>로딩중입니다1!!!!</div>: <div ref={ref}></div>}
       </div>
     );
 }
