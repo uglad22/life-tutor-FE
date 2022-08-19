8d814d783fccfc66a188e4d8e2423dd7575fc4ce
nggoong <nggoong@naver.com>
2022-08-20 02:28:10 +0900

    FEAT : 대나무숲 게시글 작성 시 추가된 해시태그를 삭제하는 기능 구현

--- src/components/hashtag/DeletableBadge.jsx
+++ src/components/hashtag/DeletableBadge.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { submitDataContext } from '../context/SubmitDataProvider';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 
-const DeletableBadge = ({ children }) => {
+
+const DeletableBadge = ({ children, idx }) => {
+    const context = useContext(submitDataContext);
+    const { postData } = context.state;
+    const { setPostData } = context.actions;
     const handleDelete = () => {
-        console.info('You clicked the delete icon.');
+        let tempHashtag = [...postData.hashtag];
+        tempHashtag.splice(idx, 1);
+        console.log(tempHashtag);
+        setPostData({...postData, hashtag:[...tempHashtag]});
+        // console.log(postData);
+        // alert(idx);
       };
     
       return (
-        <Stack direction="row" spacing={1}>
-          <Chip label={ children } onDelete={handleDelete} />
+        <Stack direction="row" spacing={1} >
+          <Chip label={ children } onDelete={handleDelete} sx={{background:"#DFE2FF"}}/>
         </Stack>
       );
 }
