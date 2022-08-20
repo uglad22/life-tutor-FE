c9a685be0ba07a0af0dd1f7872d108a18a7d9024
nggoong <nggoong@naver.com>
2022-08-20 13:41:44 +0900

    FEAT : 브라우저 새로고침 시 하단 네비게이션 바 아이콘 포커즈 유지하도록 구현

---
+++ src/components/nav/NavIndexInfo.js
@@ -0,0 +1,20 @@
+const IndexInfo = [
+    {
+        path:"/home/postings",
+        idx:0
+    },
+    {
+        path:"home/rooms",
+        idx:1
+    },
+    {
+        path:"/posting",
+        idx:2
+    },
+    {
+        path:"/mypage",
+        idx:3
+    }
+]
+
+export default IndexInfo;
