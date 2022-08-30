094fcd6751b8b1ec2987d5e45d1d72a9a4e32e28
s-woon <sus9763@gmail.com>
2022-08-30 16:14:35 +0900

    FEAT: 유저 패스워드 변경 페이지 추가 및 기능 구현

---
+++ src/pages/MyPwManage.jsx
@@ -0,0 +1,148 @@
+import React, { useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from '../components/header/Header';
+import instance from '../shared/axios';
+
+const MyPwManage = () => {
+    const _pwcheck = /^[0-9a-zA-Z!@#$%^&.*]{4,20}$/;
+    const nowPw_ref = useRef();
+    const newPw_ref = useRef();
+    const newConfirmPw_ref = useRef();
+    const navigate = useNavigate();
+
+    const [ newPwCheck, setNewPwCheck ] = useState(false);
+    const [ newPwConfirmCheck, setNewPwConfirmCheck ] = useState(false);
+
+    const newPwCheckHandler = () => {
+        if (_pwcheck.test(newPw_ref.current.value)) {
+            setNewPwCheck(true);
+        } else {
+            setNewPwCheck(false);
+        }
+    }
+
+    const newPwConfirmCheckHandler = () => {
+        if (newPw_ref.current.value === newConfirmPw_ref.current.value) {
+            setNewPwConfirmCheck(true);
+        } else {
+            setNewPwConfirmCheck(false);
+        }
+    }
+
+    console.log(newPwCheck, newPwConfirmCheck);
+
+    const myPwChangeHandler = async () => {
+        if (newPwCheck && newPwConfirmCheck) {
+            const data = {
+                password: nowPw_ref.current.value,
+                changePassword: newPw_ref.current.value,
+                confirmChangePassword: newConfirmPw_ref.current.value
+            }
+            try {
+                const res = await instance.put("/api/mypage/user/password", data);
+                console.log(res);
+                alert('비밀번호를 변경하였습니다.');
+                navigate("/mypage");
+            } catch (err) {
+                console.log(err);
+                alert('비밀번호가 일치하지 않습니다.');
+            }
+        } else {
+            alert('변경하려는 비밀번호가 일치하지 않습니다.');
+        }
+    }
+
+  return (
+    <>
+      <Header title="비밀번호 변경" isAction={true} />
+      <MyPwWrapper>
+        <NowPwBox>
+          <p>현재 비밀번호</p>
+          <input ref={nowPw_ref} type="password"></input>
+        </NowPwBox>
+        <NewPwBox>
+          <p>새 비밀번호</p>
+          <input ref={newPw_ref} type="password" placeholder="영문 8-20자, 특수문자(!@#$%^&.*)포함" onChange={() => {newPwCheckHandler()}}></input>
+        </NewPwBox>
+        <NewPwConfirmBox>
+          <p>새 비밀번호 확인</p>
+          <input ref={newConfirmPw_ref} type="password" placeholder="비밀번호 확인" onChange={() => {newPwConfirmCheckHandler()}}></input>
+        </NewPwConfirmBox>
+        <ChangeMyPwBtn onClick={() => {myPwChangeHandler()}}>변경하기</ChangeMyPwBtn>
+      </MyPwWrapper>
+    </>
+  );
+};
+
+export default MyPwManage;
+
+const MyPwWrapper = styled.div`
+  margin-top: 60px;
+  background: white;
+  width: 100%;
+  height: calc(100% - 60px - 71px);
+  position: fixed;
+  color: #757575;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  input {
+    box-sizing: border-box;
+    height: 46px;
+    background: #ffffff;
+    border: 1.5px solid #d8d8d8;
+    border-radius: 8px;
+    padding: 14px;
+    ::placeholder {
+      color: #757575;
+    }
+  }
+`;
+
+const NowPwBox = styled.div`
+  width: 336px;
+  margin: 27.5px auto 0px;
+  display: flex;
+  flex-direction: column;
+  p {
+    font-size: 12px;
+    margin-bottom: 8px;
+  }
+`;
+
+const NewPwBox = styled.div`
+  width: 336px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  p {
+    font-size: 12px;
+    margin-bottom: 8px;
+  }
+`;
+
+const NewPwConfirmBox = styled.div`
+  width: 336px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  p {
+    font-size: 12px;
+    margin-bottom: 8px;
+  }
+`;
+
+const ChangeMyPwBtn = styled.div`
+    width: 335px;
+    margin: 310px auto 0px;
+    background: #3549FF;
+    color: white;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 18px;
+    font-weight: bold;
+    border-radius: 40px;
+`;
