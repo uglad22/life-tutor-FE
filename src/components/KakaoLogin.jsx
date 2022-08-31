481583a027bf869bbd00af7feb86bb5864127126
uglad22 <dyhh12@naver.com>
2022-08-31 16:05:11 +0900

    카카오 로그인 유저 토큰 문제 수정

--- src/components/KakaoLogin.jsx
+++ src/components/KakaoLogin.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router-dom";
 
 function KakaoLogin() {
   const params = useParams();
+  const navigate = useNavigate();
   
   useEffect(() => {
-  if(params.token == "fail"){
-  alert("로그인에 실패했습니다ㅠㅠ 카카오 이메일 제공 동의가 필요합니다!")
-  window.location.replace("/login");
+  if(params.token === "fail"){
+    alert("로그인에 실패했습니다ㅠㅠ 카카오 이메일 제공 동의가 필요합니다!")
+    navigate('/login');
   } else {
-    localStorage.clear();
-    localStorage.setItem("token", `BEARER ${params.token}`);
-    
-    // 사용하려면 App.js에서 /로 라우팅해야 한다
-    window.location.replace("/home");}
+    localStorage.setItem("Authorization", `BEARER ${params.token}`);
+    console.log(localStorage.Authorization);
+    navigate('/viewer/posting/list')
+  
+  };
   
 
   }, []);
