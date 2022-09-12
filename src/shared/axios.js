7f1d1634efcfedb20913215c1f538edc35ee3cc4
nggoong <nggoong@naver.com>
2022-09-12 11:51:40 +0900

    FEAT : 리프레시 토큰 구현 완료

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -21,17 +21,27 @@ instance.interceptors.response.use((res) => {
     return res;
 }, (err) => {
     if(err.response && err.response.status === 401) {
-        console.log("access_token 갱신!");
-        const refreshToken = sessionStorage.getItem("refresh__token");
+        const refreshToken = sessionStorage.getItem("Refresh__Token");
+        const prevAccessToken = sessionStorage.getItem("Authorization");
         const originRequest = err.config; // 원래의 요청
 
-        return axios.get(`${process.env.REACT_APP_API_URL}/api/${refreshToken}`).then((res) => {
+        return axios.put(`${process.env.REACT_APP_API_URL}/api/refreshToken`,{}, {
+            headers: {
+                Authorization:prevAccessToken,
+                RefreshToken:refreshToken
+            }
+        }).then((res) => {
             const newAccessToken = res.headers.authorization;
             sessionStorage.setItem("Authorization", newAccessToken);
             originRequest.headers.Authorization= newAccessToken;
 
-            return instance.request(originRequest); // 원래의 요청으로 다시 요청
+            return instance.request(originRequest).catch((e) => {
+                alert("세션이 만료되었습니다.");
+                console.log(e);
+                window.location.href="/login";
+            }); // 원래의 요청으로 다시 요청
         }).catch((err) => {
+            alert("세션이 만료되었습니다.")
             sessionStorage.removeItem("Authorization");
             window.location.href="/login";
         })
